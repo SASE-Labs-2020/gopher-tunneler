549689f9412f1483fb3ba3a7c40ec0994d5652b8
Your Name <nathanielbd@gmail.com>
2020-08-18 15:00:49 -0500

    try with react-mapbox-gl

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import ReactMapboxGl, {
   ScaleControl,
   ZoomControl,
-  RotationControl,
   Layer,
   Feature
 } from 'react-mapbox-gl';
+import Spinner from 'react-bootstrap/Spinner';
 
 const GlMap = ReactMapboxGl({ accessToken: { token: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA' }})
+const lineLayout = {
+  'line-cap': 'round',
+  'line-join': 'round'
+};
+
+const linePaint = {
+  'line-color': '#4790E5',
+  'line-width': 12
+};
 
 export default class Map extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { data: [] };
+		this.state = { data: [], isLoading: true };
 	}
 
 	async getData(url) {
@@ -20,10 +29,10 @@ export default class Map extends Component {
 		return response.json();
 	}
 
-	async componentDidMount() {
+	async componentWillMount() {
 		var urls;
 		const Dijkstra = require('node-dijkstra');
-		const graph = await this.getData('https://SASE-Labs-2020.github.io/assets/graph.json')
+		const graph = await this.getData('https://SASE-Labs-2020.github.io/assets/graph.json');
 		const edsger = new Dijkstra(graph);
 		const names = await this.getData('https://SASE-Labs-2020.github.io/assets/names.json');
 		if (this.props.start == null) {
@@ -44,6 +53,12 @@ export default class Map extends Component {
 				(paths) => paths.map(
 					(path) => {
 						const new_path = [names[path[0]], names[path[1]]];
+						if (!names[path[0]]) {
+							console.log(path[0]);
+						}
+						if (!names[path[1]]) {
+							console.log(path[1]);
+						}
 						return 'https://SASE-Labs-2020.github.io/assets/directions/' + new_path.join('_') + '.json';
 					}
 				)
@@ -63,15 +78,19 @@ export default class Map extends Component {
 				this.setState(
 					(prevState) => {
 						return {
-							data: prevState.data.concat(responseData)
+							data: prevState.data.concat(responseData),
 						};
 					}
 				);
 			});
 		});
+		this.setState({ isLoading: false });
 	}
 
 	render() {
+		if (this.state.isLoading) {
+			return <Spinner animation="border" variant="dark"/>;
+		}
 		return (
 			<GlMap
 				center={[44.974208, -93.2325]}
@@ -80,7 +99,7 @@ export default class Map extends Component {
 				<ScaleControl/>
 				<ZoomControl/>
 				{this.state.data.map(json => 
-					<Layer type="line">
+					<Layer type="line" layout={lineLayout} paint={linePaint}>
 						<Feature coordinates={json.coordinates.map(point => [point.longitude, point.latitude])}/>
 					</Layer>
 				)}
