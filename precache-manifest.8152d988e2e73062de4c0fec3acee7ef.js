fc142e57c11d46c053c8a5b4b7ed0f5b37600e89
Your Name <nathanielbd@gmail.com>
2020-08-19 18:10:00 -0500

    Updates

---
+++ precache-manifest.8152d988e2e73062de4c0fec3acee7ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5596ce172d726cdf76c8d60de81cb35a",
+    "url": "/gopher-tunneler/index.html"
+  },
+  {
+    "revision": "f044d708c59b628fe6da",
+    "url": "/gopher-tunneler/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "91f765626eb12f15c875",
+    "url": "/gopher-tunneler/static/css/main.3c93b688.chunk.css"
+  },
+  {
+    "revision": "f044d708c59b628fe6da",
+    "url": "/gopher-tunneler/static/js/2.36ab2af8.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/gopher-tunneler/static/js/2.36ab2af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91f765626eb12f15c875",
+    "url": "/gopher-tunneler/static/js/main.dafbf576.chunk.js"
+  },
+  {
+    "revision": "128d63b9752d672be0ee",
+    "url": "/gopher-tunneler/static/js/runtime-main.ac54bd87.js"
+  }
+]);
