ff6d1ad393b5de28c5cf6e78c408ecb37a01c13d
Abigail Chiaokhiao <abi.chi703@gmail.com>
2020-08-19 22:01:43 -0500

    Formatting Try 1

--- src/components/Contributing.js
+++ src/components/Contributing.js
@@ -12,15 +12,22 @@ export default class Contributing extends Component {
 			<Card text="dark">
 				<Card.Body>
 					<Card.Title>Contributing</Card.Title>
-					<Card.Subtitle className="mb-2 text-muted">Do you know something about the Gopher Way not included on the app? YOU can add to it!</Card.Subtitle>
+					<Card.Subtitle className="mb-2 text-muted">Know something about the Gopher Way not included yet? YOU can add to it!</Card.Subtitle>
 					<Card.Text>
-The preferred way of contributing is to create an issue on GitHub. You do not need any programming knowledge to do this. All you need is an email to create an account.
+						Option 1 (preferred): Create a GitHub Issue
 					</Card.Text>
-					<Button variant="secondary" href="https://github.com/SASE-Labs-2020/SASE-Labs-2020.github.io/issues/new">Create an issue</Button>
+					<Card.Subtitle className="mb-2 text-muted">
+						No programming knowledge needed. Only an email for an account.
+					</Card.Subtitle>
+					<Button variant="secondary" href="https://github.com/SASE-Labs-2020/SASE-Labs-2020.github.io/issues/new">Create an Issue</Button>
+					
 					<Card.Text>
-If you do not want to create a GitHub account, you may submit an anonymous Google form.
+						Option 2: Submit a Google Form
 					</Card.Text>
-					<Button variant="primary" href="https://forms.gle/PUGtoRsrAP3jrcj67">Submit the form</Button>
+					<Card.Subtitle className="mb-2 text-muted">
+						If you would prefer not to make a GitHub account, we also have an anonymous form.
+					</Card.Subtitle>
+					<Button variant="secondary" href="https://forms.gle/PUGtoRsrAP3jrcj67">Submit the Form</Button>
 				</Card.Body>
 			</Card>
 		)
