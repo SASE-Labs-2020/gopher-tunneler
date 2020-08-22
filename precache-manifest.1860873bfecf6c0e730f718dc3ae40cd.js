2497f5617a33827095a66fafec4d312848363b47
Your Name <nathanielbd@gmail.com>
2020-08-22 17:41:08 -0500

    Updates

--- precache-manifest.1860873bfecf6c0e730f718dc3ae40cd.js
+++ precache-manifest.1860873bfecf6c0e730f718dc3ae40cd.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4f0b557626ddf88f51da3b4459d463cc",
+    "revision": "ea7da98ecf2f223aa77b0e61ff1dcd07",
     "url": "/gopher-tunneler/index.html"
   },
   {
-    "revision": "4ab816e49c539123217d",
+    "revision": "1c86063dc436cb306a7f",
     "url": "/gopher-tunneler/static/css/2.7c35e809.chunk.css"
   },
   {
-    "revision": "642298d4ce146bd1142d",
-    "url": "/gopher-tunneler/static/css/main.233472bd.chunk.css"
+    "revision": "f0b542f3d31e3413d3c3",
+    "url": "/gopher-tunneler/static/css/main.69a5c4ed.chunk.css"
   },
   {
-    "revision": "4ab816e49c539123217d",
-    "url": "/gopher-tunneler/static/js/2.82e5c153.chunk.js"
+    "revision": "1c86063dc436cb306a7f",
+    "url": "/gopher-tunneler/static/js/2.79cd6b37.chunk.js"
   },
   {
     "revision": "6791e7fb9584d65ed87d655035c7da95",
-    "url": "/gopher-tunneler/static/js/2.82e5c153.chunk.js.LICENSE.txt"
+    "url": "/gopher-tunneler/static/js/2.79cd6b37.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "642298d4ce146bd1142d",
-    "url": "/gopher-tunneler/static/js/main.2bea6415.chunk.js"
+    "revision": "f0b542f3d31e3413d3c3",
+    "url": "/gopher-tunneler/static/js/main.fa730237.chunk.js"
   },
   {
     "revision": "128d63b9752d672be0ee",
